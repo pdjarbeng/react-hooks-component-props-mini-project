df50592a0b135ff3fa5cf94e7d4f309f1eac2e10
priscilla <pdjarbeng@outlook.com>
2021-11-06 22:48:45 -0400

    commit tree dfa493845e18b79e71793c3ef4f1b6a368f63a85

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// Make a Header component as a child of App. It should return:
+// a <header> element with the following elements inside:
+// an <h1> with the name of the blog, passed as a prop
+
+function Header(props) {
+    return (
+      <header>
+          <h1>Underreacted</h1>
+      </header>
+    );
+  }
+
+  export default Header
